1ed6c31dd27480b78827d23edc3112824a8753d5
Shamiul Alam <1hourprograms@gmail.com>
2022-08-01 08:16:02 +0600

    Second 1st

--- app.py
+++ app.py
@@ -13,8 +13,10 @@ class Owner(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(20))
     address = db.Column(db.String(100))
+    pets = db.relationship('Pet', backref='owner')
 
 class Pet(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(20))
-    age = db.Column(db.Integer)
+    age = db.Column(db.String(100))
+    owner_id = db.Column(db.Integer, db.ForeignKey('owner.id'))
